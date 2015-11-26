85138b73e00473a946046d3e180c79e6edc516c7
ethan <erogers6264@gmail.com>
2015-11-26 00:17:49 -0700

    Debug

--- flompr/flompr.py
+++ flompr/flompr.py
@@ -56,7 +56,7 @@ def login():
 	if request.method == 'POST':
 		if request.form['username'] != app.config['USERNAME']:
 			error = 'Invalid username'
-		elif: request.form['password'] != app.config['PASSWORD']:
+		elif request.form['password'] != app.config['PASSWORD']:
 			error = 'Invalid password'
 		else:
 			session['logged_in'] = True
