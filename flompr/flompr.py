231c8d0d82e6251b06c2e0c436dfe614151c58b5
ethan <erogers6264@gmail.com>
2015-11-25 22:53:06 -0700

    Connect to db before requests
    Close after

--- flompr/flompr.py
+++ flompr/flompr.py
@@ -18,11 +18,22 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-def init_db:
+def init_db():
 	with closing(connect_db()) as db:
 		with app.open_resource('flompr.sql', mode='r') as f:
 			db.cursor().executescript(f.read())
 		db.commit()
 
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
+
+# run as standalone app
 if __name__ == '__main__':
 	app.run()
